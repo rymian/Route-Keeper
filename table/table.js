35a896739255bad969411f9e20efa4dc26b295fb
ryanmanderson <anderson.m.ryan@icloud.com>
2019-12-11 11:44:41 -0500

    Adds functionality to the filter buttons

--- table/table.js
+++ table/table.js
@@ -1,9 +1,11 @@
 import Route from "../route.js";
-import {setDetail} from "../detail/detail.js"
+import {setDetail} from "../detail/detail.js";
+import {getFilter} from "../filter/filter.js";
 
 const table = $('#table-container');
 
-var routes;
+var allRoutes;
+var filteredRoutes;
 var lastSelectedId = 0;
 
 /* Render RouteCells */
@@ -28,47 +30,79 @@ const renderRouteCell = function(route) {
 
 /* Loads RouteCells into the table */
 
-const loadRouteCells = function() {
-    // fetchRoutes().forEach(route => {
-    //     appendRoute(route);
-    // })
-
-    // setDetail(routes[0]);
-};
-
 const fetchRoutes = function() {
-    routes = [];
+    allRoutes = [];
+    filteredRoutes = [];
 
     for(var i=0; i < 36; i++) {
         let route = new Route(`Route #${i+1}`, [[0.0,0.0]], 'Location', 0, 0);
         route.id = i;
-        routes.push(route);
+        allRoutes.push(route);
     }
 
-    return routes;
+    loadTable();
 }
+
+export const loadTable = function() {
+    if (allRoutes != null) {
+        table.empty();
+        filteredRoutes = [];
+
+        allRoutes.forEach(route => {
+            if (filter(route)) {
+                filteredRoutes.push(route);
+                appendRoute(route);
+            }
+        })
+
+        if (filteredRoutes.length != 0) {
+            setDetail(filteredRoutes[0]);
+        }
+    }
+};
 
 const appendRoute = function(route) {
     let cell = renderRouteCell(route);
-        table.append(cell);
-        table.on("click",".route-cell-card", selectRouteCell);
+    table.append(cell);
+    table.on("click",".route-cell-card", selectRouteCell);
 }
 
 export const prependRoute = function(route) {
-    if (routes == null) {
-        routes = [];
+    if (filteredRoutes == null) {
+        allRoutes = []
+        filteredRoutes = [];
         route.id = 0;
     } else {
-        route.id = routes.length;
+        route.id = allRoutes.length;
     }
-    routes.push(route);
+    allRoutes.push(route);
 
-    let cell = renderRouteCell(route);
-    table.prepend(cell);
-    table.on("click",".route-cell-card", selectRouteCell);
+    if (filter(route)) {
+        let cell = renderRouteCell(route);
+        table.prepend(cell);
+        table.on("click",".route-cell-card", selectRouteCell);
 
-    lastSelectedId = route.id;
-    setDetail(route);
+        lastSelectedId = route.id;
+        setDetail(route);
+    }
+}
+
+const filter = function(route) {
+    switch (getFilter()) {
+        case "results":
+            return true;
+            break;
+        case "favorites":
+            // TODO: use actual user name
+            return route.favorites.includes("user");
+            break;
+        case "you":
+            // TODO: use actual user name
+            return route.user === "user";
+            break;
+    }
+
+    return false;
 }
 
 /* Actions */
@@ -77,7 +111,7 @@ const selectRouteCell = function(event) {
     let id = parseInt(this.id, 10);
 
     if (lastSelectedId != id) {
-        let route = routes.filter(route => {
+        let route = filteredRoutes.filter(route => {
             return route.id == id;
         })[0]
 
@@ -87,5 +121,5 @@ const selectRouteCell = function(event) {
 };
 
 $(function() {
-    loadRouteCells();
+    fetchRoutes()
 });
